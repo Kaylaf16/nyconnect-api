267eb04db07403e4fd451b98ab456a06f7c3b033
Hashir Qureshi <qureshihashir@yahoo.com>
2018-10-13 04:37:04 -0400

    started working on the map api file

---
+++ controllers/map.js
@@ -0,0 +1,4 @@
+const router = require("./index");
+const user = require("../server/models/user");
+const skill = require("../server/models/skill");
+const userSkills = require("../server/models/user_skills");
