c494a929473537eaf5db03f3d7ba1158abf8bdf1
Hashir Qureshi <qureshihashir@yahoo.com>
2018-10-13 08:04:12 -0400

    added lattitude and longitude

--- controllers/map.js
+++ controllers/map.js
@@ -12,7 +12,7 @@ router.get("/filterUsers", (req, res) => {
   console.log(lat);
   console.log(long);
   const query =
-    `SELECT min(u.id) as id, u.name, group_concat(s.skill) as skills, min(( 3959 * acos( cos( radians(${parseFloat(lat)}) ) * cos` +
+    `SELECT min(u.id) as id, u.name, min(substring_index(u.location, ',', 1)) as lat, min(substring_index(u.location, ',', -1)) as lon, group_concat(s.skill) as skills, min(( 3959 * acos( cos( radians(${parseFloat(lat)}) ) * cos` +
     `( radians( cast(substring_index(u.location, ',', 1) as decimal(10,6)) ) ) * cos( radians( cast(substring_index(u.location, ',', -1)`+
     ` as decimal(10,6)) ) - radians(${parseFloat(long)}) ) + sin( radians(${parseFloat(lat)}) ) * sin( radians( cast(substring_index(u.location, ',', 1)` +
     " as decimal(10,6)) ) ) ) )) AS distance FROM user u" +
@@ -22,5 +22,22 @@ router.get("/filterUsers", (req, res) => {
     return res.json(users);
   });
 });
+
+// router.get("/filterUsers", (req, res) => {
+//   const {radius=10, lat, long} = req.query;
+//   console.log(radius);
+//   console.log(lat);
+//   console.log(long);
+//   const query =
+//     `SELECT min(u.id) as id, u.name, group_concat(s.skill) as skills, min(( 3959 * acos( cos( radians(${parseFloat(lat)}) ) * cos` +
+//     `( radians( cast(substring_index(u.location, ',', 1) as decimal(10,6)) ) ) * cos( radians( cast(substring_index(u.location, ',', -1)`+
+//     ` as decimal(10,6)) ) - radians(${parseFloat(long)}) ) + sin( radians(${parseFloat(lat)}) ) * sin( radians( cast(substring_index(u.location, ',', 1)` +
+//     " as decimal(10,6)) ) ) ) )) AS distance FROM user u" +
+//     ` inner join user_skills us on u.id = us.user_id inner join skill s on s.id = us.skill_id group by u.name HAVING distance < ${parseInt(radius)} order by` +" distance;";
+
+//   db.query(query, { type: db.QueryTypes.SELECT }).then(users => {
+//     return res.json(users);
+//   });
+// });
 
 module.exports = router;
